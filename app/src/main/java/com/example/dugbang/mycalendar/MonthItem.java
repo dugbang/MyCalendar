e9d56099ef401bfdac1cef7bb08b402e67f77c4e
dugbang <dugbang@gmail.com>
2017-10-22 21:04:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dugbang/mycalendar/MonthItem.java
@@ -0,0 +1,14 @@
+package com.example.dugbang.mycalendar;
+
+/**
+ * Created by dugbang on 2017-10-21.
+ */
+
+public class MonthItem {
+
+    int day;
+
+    public MonthItem(int day) {
+        this.day = day;
+    }
+}
